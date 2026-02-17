6a62c86056d9828c48a18e05e1e2b7f615d66b62
Fluster.IO <aiglinski414@gmail.com>
2026-02-16 18:24:11 -0600

    Fix build script for website to build utils package first

--- apps/website/src/app/docs/[[...slug]]/client_mdx.tsx
+++ apps/website/src/app/docs/[[...slug]]/client_mdx.tsx
@@ -1,3 +1,18 @@
 "use client";
+import dynamic from "next/dynamic";
 
-export { Admonition, Ul, Hl } from "@fluster/webview_utils";
+const Admonition = dynamic(
+    () => import("@fluster/webview_utils").then((a) => a.Admonition),
+    {
+        ssr: false,
+    },
+);
+const Hl = dynamic(() => import("@fluster/webview_utils").then((a) => a.Hl), {
+    ssr: false,
+});
+
+const Ul = dynamic(() => import("@fluster/webview_utils").then((a) => a.Ul), {
+    ssr: false,
+});
+
+export { Admonition, Ul, Hl };
