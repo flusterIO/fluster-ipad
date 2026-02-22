5f61ec66f5f94b49105fe092d4e7e64b954b0e15
Fluster.IO <aiglinski414@gmail.com>
2026-02-22 17:15:38 -0600

    iPad paper pagination working

--- apps/fluster/Fluster/Features/paper/views/paper_tab_view/paper_kit/paper_kit_view.swift
+++ apps/fluster/Fluster/Features/paper/views/paper_tab_view/paper_kit/paper_kit_view.swift
@@ -6,28 +6,50 @@
 //
 
 import FlusterData
+import FlusterSwift
 import PaperKit
 import SwiftUI
 
 struct PaperKitView: View {
   @Binding public var editingNote: NoteModel
-  @State private var documentData = Data()
+  @Binding public var focusedPageIndex: Int
+  @State private var data: Data = Data()
 
   var body: some View {
     GeometryReader { geometry in
-      NavigationStack {
-        PaperCanvasView(
-          canvasData: $documentData,
-//          canvasBounds: CGRect(origin: .zero, size: geometry.size)
-        )
-        .ignoresSafeArea(edges: .bottom)
-        .navigationTitle("iPad Markup")
-        .navigationBarTitleDisplayMode(.inline)
-        .onDisappear {
-          // In a real app, you would pass a save action via an Environment
-          // key or trigger the Coordinator's save method before dismissing.
-          print("Canvas dismissed. Trigger save sequence here.")
+      if focusedPageIndex <= editingNote.paper.count - 1 {
+        if let ep = Binding(Binding($editingNote.paper[focusedPageIndex].markup)) {
+          NavigationStack {
+            PaperCanvasView(
+              canvasData: ep,
+              canvasBounds: CGRect(origin: .zero, size: CGSize(width: geometry.size.width, height: UIScreen.current?.bounds.height ?? geometry.size.height))
+            )
+            .ignoresSafeArea(edges: .bottom)
+            .navigationTitle("Page \(focusedPageIndex + 1) of \(editingNote.paper.count)")
+            //        .navigationBarTitleDisplayMode(.inline)
+            .onDisappear {
+              // In a real app, you would pass a save action via an Environment
+              // key or trigger the Coordinator's save method before dismissing.
+              print("Canvas dismissed. Trigger save sequence here.")
+            }
+          }
         }
+      } else {
+        Color.clear.task {
+          await handleFocusIndexPageCreation(geometry)
+        }
+      }
+    }
+  }
+
+  func handleFocusIndexPageCreation(_ geometry: GeometryProxy) async {
+    if focusedPageIndex >= editingNote.paper.count {
+      let markup = PaperMarkup(bounds: geometry.frame(in: .local))
+      do {
+        let data = try await markup.dataRepresentation()
+        self.editingNote.paper.append(PaperModel(markup: data))
+      } catch {
+        print("Error handling paper creation: \(error.localizedDescription)")
       }
     }
   }
---
+++ packages/swift/FlusterSwift/Sources/FlusterSwift/core/extensions/collection.swift
@@ -0,0 +1,6 @@
+extension Collection {
+    /// Returns the element at the specified index if it exists, otherwise nil.
+    subscript (safe index: Index) -> Element? {
+        return indices.contains(index) ? self[index] : nil
+    }
+}
