43132c4d94120dd8ddcef3393e3f0a528413ec78
Fluster.IO <fluster.eyeoh@gmail.com>
2026-03-13 13:41:11 -0500

    Dictionary entries in note details sheet now handling url's and note destination links concurrently

--- packages/webview_utils/src/features/bibliography/presentation/bibliography_entry.tsx
+++ packages/webview_utils/src/features/bibliography/presentation/bibliography_entry.tsx
@@ -8,39 +8,26 @@ import React, { type ReactNode } from 'react'
 interface BibliographyEntryComponentProps {
     entry: EditorCitation
     className?: string
-    idx?: {
-        index: number
-        classes?: {
-            container?: string
-            /**
-             * index is 0 based at this point.
-             */
-            index?: string
-            content?: string
-        }
-    }
     /**
      * onClick is only called when url is void
      */
     onClick?: () => void
 }
 
-export const BibliographyEntryComponent = ({ entry, onClick, className, idx }: BibliographyEntryComponentProps): ReactNode => {
-    if (idx) {
-        return (
-            <div className={cn("grid grid-cols-[64px_1fr] w-full", idx.classes?.container)}>
-                <div className={cn("text-sm place-self-center", idx.classes?.index)}>{idx.index}</div>
-                <a href={entry.url} onClick={entry.url ? undefined : onClick} className={cn("not-prose", className, idx.classes?.content)} dangerouslySetInnerHTML={{ __html: entry.html }} />
-            </div>
-        )
-    }
+export const BibliographyEntryComponent = ({ entry, onClick, className }: BibliographyEntryComponentProps): ReactNode => {
     if (entry.url) {
         return (
             <div
                 className={cn("grid grid-cols-[1fr_auto] w-full px-3 pl-2 rounded-lg cursor-pointer", className)}
             >
                 <div
-                    className={cn("not-prose w-full text-sm", entry.url ? "cursor-pointer" : "cursor-default")} dangerouslySetInnerHTML={{ __html: entry.html }} onClick={onClick}
+                    className={cn("not-prose w-full text-sm", entry.url ? "cursor-pointer" : "cursor-default")} dangerouslySetInnerHTML={{ __html: entry.html }} onClick={(e) => {
+                        e.preventDefault()
+                        e.stopPropagation()
+                        if (onClick) {
+                            onClick()
+                        }
+                    }}
                 />
                 <a
                     href={entry.url}
@@ -56,7 +43,13 @@ export const BibliographyEntryComponent = ({ entry, onClick, className, idx }: B
             role="button"
             className={cn("w-full w-full px-3 pl-2 rounded-lg text-sm [&_*]:text-sm cursor-pointer")}
             dangerouslySetInnerHTML={{ __html: entry.html }}
-            onClick={onClick}
+            onClick={(e) => {
+                e.preventDefault()
+                e.stopPropagation()
+                if (onClick) {
+                    onClick()
+                }
+            }}
         />
     )
 }
