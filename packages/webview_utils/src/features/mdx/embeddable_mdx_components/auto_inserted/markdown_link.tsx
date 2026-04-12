6fabfe31021a97a7440569440a0453e71ca6939c
Fluster.IO <fluster.eyeoh@gmail.com>
2026-04-12 18:55:28 -0500

    Link to note heading working

--- packages/webview_utils/src/features/mdx/embeddable_mdx_components/auto_inserted/markdown_link.tsx
+++ packages/webview_utils/src/features/mdx/embeddable_mdx_components/auto_inserted/markdown_link.tsx
@@ -1,6 +1,6 @@
 import React, { type ReactNode } from 'react'
 import { type MarkdownLinkResult } from "@/code_gen/typeshare/conundrum"
-
+import { useSendNotificationBanner } from '#/notifications/splitview_editor_notification_banner/send_splitview_notification_banner'
 
 
 interface AutoInsertedMarkdownLinkProps {
@@ -8,8 +8,33 @@ interface AutoInsertedMarkdownLinkProps {
 }
 
 export const AutoInsertedMarkdownLink = ({ data }: AutoInsertedMarkdownLinkProps): ReactNode => {
+    const sendNotif = useSendNotificationBanner()
+    if (data.url.startsWith("#")) {
+        return <a role="button" className="cursor-pointer" onClick={() => {
+            const domId = data.url.replace("#", "");
+            const heading = document.getElementById(`h-${domId}`);
+            if (heading) {
+                heading.scrollIntoView({
+                    behavior: "smooth"
+                })
+            } else {
+                const em = document.getElementById(domId);
+                if (em) {
+                    em.scrollIntoView({
+                        behavior: "smooth"
+                    })
+                } else {
+                    sendNotif({
+                        title: "Not found",
+                        body: "An element with that id could not be found on the current page.",
+                        timeout: 5000
+                    })
+                }
+            }
+        }}>{data.text}</a>
+    }
     return (
-        <a href={data.url}>{data.text}</a>
+        <a className="cursor-pointer" href={data.url}>{data.text}</a>
     )
 }
 
